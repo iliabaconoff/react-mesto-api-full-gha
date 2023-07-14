2a436507fb9de3993e169e914be7ba1183ec4fec
Ilia Ivashkin <lilwoozzy@gmail.com>
2023-07-15 06:12:33 +0800

    Update Card.jsx

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React, { useContext} from "react";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
-const Card = ( cardData, likes, name, link, onCardClick, onCardLike, onCardDelete ) => {
+const Card = ( cardData, card, likes, name, link, onCardClick, onCardLike, onCardDelete ) => {
 
   const currentUser = useContext(CurrentUserContext);
   const isOwn = cardData.owner === currentUser._id;
-  const isLiked = likes.some((user) => user === currentUser._id);
-  const cardLikeButtonClassName = (`card__like ${isLiked && 'card__like_pressed'}`);;
+  const isLiked = card.likes.some((user) => user === currentUser._id);
+  const cardLikeButtonClassName = (`card__like ${isLiked && 'card__like_pressed'}`);
 
   function handleClick() {
     onCardClick(cardData)
